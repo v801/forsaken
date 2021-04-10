c8f79f44f5aad22971ea118ad75d7c9e2d31231c
v801 <32247825+v801@users.noreply.github.com>
2021-04-10 02:34:18 -0700

    update assemblyinfo

--- ForsakenMod/Properties/AssemblyInfo.cs
+++ ForsakenMod/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("CrushAntlers")]
+[assembly: AssemblyTitle("Forsaken")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("CrushAntlers")]
-[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyProduct("")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
